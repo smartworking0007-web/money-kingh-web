ac5a9e452f0fd1bedb80590c003094b5842dc66a
sumitmishra84 <sm0141136@gmail.com>
2026-01-24 12:41:01 +0530

    added redirect logic to next.config.ts

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -3,6 +3,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  // Ye code www wali site ko non-www (Money King) par redirect karega
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'www.moneykingfinancial.com',
+          },
+        ],
+        destination: 'https://moneykingfinancial.com/:path*',
+        permanent: true, // 301 Redirect: Google ko batata hai ki sirf ek hi site hai
+      },
+    ];
+  },
 };
 
 export default nextConfig;
